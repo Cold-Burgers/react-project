2ca5112a6f798a9d10931f888ef637c3d818829e
Cold-Burgers <cgburges@lakeheadu.ca>
2021-03-08 22:33:20 -0500

    Adjusted text on about/home

--- src/Components/pages/About.jsx
+++ src/Components/pages/About.jsx
@@ -5,10 +5,10 @@ const About = () => {
         <div>
             <Header title="About"/>
             <div className="paragraph-text">
-                <p>My application has functionality to search for keywords in article headlines, as well as the ability to sort the data alphabetically.
-                For searching / filtering by keyword, the user just needs to type in a keyword they would like to see news for and the page will update accordingly.
-                To sort the user will click a button, and then the stories will be sorted alphabetically. They can then reverse the sort.
-                Regretably, I couldn't get the sort and the search to work together on the same page so I have made two seperate ones. Just to show both operations.</p>
+                <p>My application has functionality to search for keywords in article headlines, as well as the ability to sort the data alphabetically.</p>
+                <p>For searching / filtering by keyword, the user just needs to type in a keyword they would like to see news for and the page will update accordingly.
+                To sort the user will click a button, and then the stories will be sorted alphabetically. They can then reverse the sort. </p>
+                <p>Regretably, I couldn't get the sort and the search to work together on the same page so I have made two seperate ones. Just to show both operations.</p>
             </div>
         </div>
     ); 
--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <div className="paragraph-text">
                 <div >
                     <p>My application is one for viewing news stories taken from <a href="https://newsapi.org/docs/endpoints/top-headlines">https://newsapi.org/docs/endpoints/top-headlines</a>
-                    It ended up being easier to (in my opinion) to use data from a local file than live from an api, so I put data from an api call in a json file locally, I was also getting rate limited.</p>
+                    It ended up being easier to (in my opinion) to use data from a local file than live from an api, so I put data from an api call in a json file locally (I was also getting rate limited). </p>
                 </div>
             </div>
         </div>
